80b44197ee6ff43d72fdd046befb45d6d6a10f4c
thej14hugar <Thejashugar@gmail.com>
2026-04-14 16:33:59 +0530

    Add new input validation functions

--- validation/input_validation.h
+++ validation/input_validation.h
@@ -2,5 +2,8 @@
 #define INPUT_VALIDATION_H
 
 int validate_input_file(const char *filename);
+int is_strict_integer(const char *str);
+int parse_integer(char *line, int *out);
+int parse_pdu_size(const char *line);
 
 #endif
